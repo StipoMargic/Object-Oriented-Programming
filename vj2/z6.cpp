bd92442b46d47b38d1a8fcd433fc0a0a86d435e7
Stipo Margic <stipo@liberato.io>
2020-03-18 11:34:41 +0100

    z6 fixed?

--- vj2/z6.cpp
+++ vj2/z6.cpp
@@ -10,9 +10,10 @@ typedef struct vector
   int *arr;
 } vector;
 
-vector newVector(vector Vector)
+vector newVector()
 {
-  Vector.arr = (int *)malloc (sizeof(int) * Vector.init);
+  vector Vector;
+  Vector.arr = (int *)malloc(sizeof(int) * Vector.init);
 
   return Vector;
 }
@@ -22,28 +23,53 @@ int vectorSize(vector Vector)
   return Vector.logicalSize;
 }
 
-vector vectorDelete(vector Vector)
+void vectorDelete(vector &Vector)
 {
   free(Vector.arr);
 }
 
-int *vectorFront(vector Vector)
+int vectorFront(vector &Vector)
 {
-  return &Vector.arr[0];
+  return Vector.arr[0];
 }
 
-int *vectorBack(vector Vector)
+int vectorBack(vector &Vector)
 {
-  return &Vector.arr[Vector.logicalSize];
+  return Vector.arr[Vector.logicalSize];
 }
 
-void vectorPop(vector Vector)
+int vectorPop(vector &Vector)
 {
+  int last = Vector.arr[Vector.logicalSize];
   Vector.logicalSize--;
+  return last;
 }
 
-void vectorPush(vector Vector, int n)
+void vectorPush(vector &Vector, int n)
 {
-  Vector.logicalSize = n;
+
+  Vector.init = Vector.init * 2;
+  Vector.arr = (int *)realloc(Vector.arr, sizeof(int) * Vector.init);
+  Vector.arr[Vector.logicalSize] = n;
   Vector.logicalSize++;
+}
+
+int main()
+{
+  vector v1;
+  v1 = newVector();
+
+  // v1.logicalSize = 2;
+
+
+  vectorPush(v1, 1);
+  int n = vectorSize(v1);
+  cout << n << endl;
+  cout << v1.arr[0] << endl;
+  int l = vectorBack(v1);
+  cout << l << endl;
+
+  int e = vectorFront(v1);
+  cout << e << endl;
+  return 0;
 }
