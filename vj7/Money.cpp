3ced0d222fe0208a488bf7a2a1365aa67c861c2a
Stipo Margic <stipo@liberato.io>
2020-05-09 20:45:19 +0200

    exercise 8

---
+++ vj7/Money.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stipo on 09. 05. 2020..
+//
+
+#include "Money.hpp"
---
+++ vj7/Money.hpp
@@ -0,0 +1,13 @@
+//
+// Created by stipo on 09. 05. 2020..
+//
+
+#ifndef VJ7__MONEY_HPP_
+#define VJ7__MONEY_HPP_
+
+class Money
+{
+
+};
+
+#endif //VJ7__MONEY_HPP_
